d2708677700a14ab850393e5d79e1b607b52e8eb
Jean Beckan Ov <jeanbeckanov@gmail.com>
2026-02-20 11:59:51 -0500

    Fix de path

--- config/config.php
+++ config/config.php
@@ -1,9 +1,11 @@
 <?php
 declare(strict_types=1);
 
+$basePath = $_ENV['APP_BASE_PATH'] ?? getenv('APP_BASE_PATH') ?: '';
+
 return [
     'app' => [
-        'base_path' => '/SistemaDeProductos', // AJUSTA si tu carpeta se llama distinto
+        'base_path' => $basePath, // local: /SistemaDeProductos | railway: ''
     ],
     'supabase' => [
         'url' => $_ENV['SUPABASE_URL'] ?? '',
